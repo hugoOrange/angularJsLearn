df0706d6909633c9a25e09cf3acde02e582e9bc8
蔡浩 <hugohaohao@qq.com>
2019-05-31 17:21:44 +0800

    Added: Dealing with Objects that Have A Length

--- test/scope.spec.js
+++ test/scope.spec.js
@@ -1696,5 +1696,30 @@ describe("Scope", function () {
             scope.$digest();
             expect(scope.counter).toBe(2);
         });
+
+        // Dealing with Objects that Have A Length
+        it("does not consider any object with a length property an array", function () {
+            scope.obj = {
+                length: 42,
+                otherKey: 'abc'
+            };
+            scope.counter = 0;
+
+            scope.$watchCollection(
+                function (scope) {
+                    return scope.obj;
+                },
+                function (newVal, oldVal, scope) {
+                    scope.counter++;
+                }
+            );
+            
+            scope.$digest();
+
+            scope.obj.newKey = 'def';
+            scope.$digest();
+
+            expect(scope.counter).toBe(2);
+        });
     });
 });
