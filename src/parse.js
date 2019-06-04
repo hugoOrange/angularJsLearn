d07e79b0d9a7692a3d7bc8f45ddea0e8f7b79ac4
蔡浩 <hugohaohao@qq.com>
2019-06-04 14:38:19 +0800

    Added: parsing float number

--- src/parse.js
+++ src/parse.js
@@ -35,7 +35,8 @@ Lexer.prototype.lex = function (text) {
 
     while (this.index < this.text.length) {
         this.ch = this.text.charAt(this.index);
-        if (this.isNumber(this.ch)) {
+        if (this.isNumber(this.ch) || 
+            (this.ch === '.' && this.isNumber(this.peek()))) {
             this.readNumber();
         } else {
             throw "Unexpected next character: " + this.ch;
@@ -48,11 +49,16 @@ Lexer.prototype.lex = function (text) {
 Lexer.prototype.isNumber = function (ch) {
     return '0' <= ch && ch <= '9';
 };
+Lexer.prototype.peek = function () {
+    return this.index < this.text.length - 1 ?
+        this.text.charAt(this.index + 1) : 
+        false;
+}
 Lexer.prototype.readNumber = function () {
     var number = '';
     while (this.index < this.text.length) {
         var ch = this.text[this.index];
-        if (this.isNumber(ch)) {
+        if (ch === '.' || this.isNumber(ch)) {
             number += ch;
         } else {
             break;
