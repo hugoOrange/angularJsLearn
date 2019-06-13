adcd7230f54c999987dab1d7a67c85789a117315
蔡浩 <hugohaohao@qq.com>
2019-06-13 11:41:56 +0800

    Added: creats angular object once

--- test/loader.spec.js
+++ test/loader.spec.js
@@ -3,10 +3,21 @@
 "use strict";
 
 describe("setupModuleLoader", function () {
+
+    beforeEach(function () {
+        delete window.angular;
+    });
     
     it("exposes angular on the window", function () {
         setupModuleLoader(window);
         expect(window.angular).toBeDefined();
     });
-    
+
+    it("creates angular just once", function () {
+        setupModuleLoader(window);
+        var ng = window.angular;
+        setupModuleLoader(window);
+        expect(window.angular).toBe(ng);
+    });
+
 });
