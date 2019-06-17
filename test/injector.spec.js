9912c1524457552e9aa101444a28813e0a714381
蔡浩 <hugohaohao@qq.com>
2019-06-17 17:24:49 +0800

    Added: bind this in invoke

--- test/injector.spec.js
+++ test/injector.spec.js
@@ -107,5 +107,20 @@ describe("injector", function () {
             injector.invoke(fn);
         }).toThrow();
     });
+
+    // Binding `this` in Injected Functions
+    it("invokes a function with the given this context", function () {
+        var module = angular.module('myModule', []);
+        module.constant('a', 1);
+        var injector = createInjector(['myModule']);
+
+        var obj = {
+            two: 2,
+            fn: function (one) { return one + this.two; }
+        };
+        obj.fn.$inject = ['a'];
+
+        expect(injector.invoke(obj.fn, obj)).toBe(3);
+    });
     
 });
