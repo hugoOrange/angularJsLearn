aa64dcf58314bc4c0d85924269325c7c9dc6fbed
蔡浩 <hugohaohao@qq.com>
2019-06-23 18:30:08 +0800

    Added: config method: add dependencies after dependency

--- src/loader.js
+++ src/loader.js
@@ -13,16 +13,19 @@ function setupModuleLoader(window) {
 
     var angular = ensure(window, 'angular', Object);
 
-    var createModule = function (name, requires, modules) {
+    var createModule = function (name, requires, modules, configFn) {
         if (name === 'hasOwnProperty') {
             throw "hasOwnProperty is not a valid module name";
         }
+
         var invokeQueue = [];
+        var configBlocks = [];
 
         // ensure that constant inject first
-        var invokeLater = function (method, arrayMethod) {
+        var invokeLater = function (service, method, arrayMethod, queue) {
             return function () {
-                invokeQueue[arrayMethod || 'push']([method, arguments]);
+                queue = queue || invokeQueue;
+                queue[arrayMethod || 'push']([service, method, arguments]);
                 return moduleInstance;
             };
         };
@@ -30,10 +33,17 @@ function setupModuleLoader(window) {
         var moduleInstance = {
             name: name,
             requires: requires,
-            constant: invokeLater('constant', 'unshift'),
-            provider: invokeLater('provider'),
-            _invokeQueue: invokeQueue
+            constant: invokeLater('$provide', 'constant', 'unshift'),
+            provider: invokeLater('$provide', 'provider'),
+            config: invokeLater('$injector', 'invoke', 'push', configBlocks),
+            _invokeQueue: invokeQueue,
+            _configBlocks: configBlocks
         };
+
+        if (configFn) {
+            moduleInstance.config(configFn);
+        }
+
         modules[name] = moduleInstance;
         return moduleInstance;
     };
@@ -48,9 +58,9 @@ function setupModuleLoader(window) {
 
     var module = ensure(window.angular, 'module', function () {
         var modules = {};
-        return function (name, requires) {
+        return function (name, requires, configFn) {
             if (requires) {
-                return createModule(name, requires, modules);
+                return createModule(name, requires, modules, configFn);
             } else {
                 return gotModule(name, modules);
             }
