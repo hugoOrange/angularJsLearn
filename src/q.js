0aec86fd2faaa6e17b45df836670d29166f05378
蔡浩 <hugohaohao@qq.com>
2019-07-16 21:29:05 +0800

    Added: defer() return Deferred with Promise

--- src/q.js
+++ src/q.js
@@ -3,6 +3,21 @@
 
 function $QProvider() {
     this.$get = function () {
-        
+
+        function Promise() {
+            
+        }
+
+        function Deferred() {
+            this.promise = new Promise();
+        }
+
+        function defer() {
+            return new Deferred();
+        }
+
+        return {
+            defer: defer
+        };
     };
 }
---
+++ test/q.spec.js
@@ -0,0 +1,24 @@
+/* jshint globalstrict: true */
+/* global publishExternalAPI: false, createInjector: false */
+'use strict';
+
+describe("$q", function () {
+    
+    var $q;
+
+    beforeEach(function () {
+        publishExternalAPI();
+        $q = createInjector(['ng']).get('$q');
+    });
+
+    it("can create a Deferred", function () {
+        var d = $q.defer();
+        expect(d).toBeDefined();
+    });
+
+    it("has a promise for each Deferred", function () {
+        var d = $q.defer();
+        expect(d.promise).toBeDefined();
+    });
+
+});
