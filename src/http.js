b989e6a87470e6f720f3d70c8d5829a9c41024a8
蔡浩 <hugohaohao@qq.com>
2019-10-15 19:49:46 +0800

    Added: request timeout -- support promise & number

--- src/http.js
+++ src/http.js
@@ -178,6 +178,7 @@ function $HttpProvider() {
                 reqData,
                 done,
                 config.headers,
+                config.timeout,
                 config.withCredentials
             );
             return deferred.promise;
