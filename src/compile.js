5a91e4d04cb9c4648c736a0cfdce2736399e3ce4
蔡浩 <hugohaohao@qq.com>
2019-10-28 19:22:21 +0800

    Added: compilation terminal

--- src/compile.js
+++ src/compile.js
@@ -44,20 +44,31 @@ function $CompileProvider($provide) {
         function compileNodes($compileNodes) {
             _.forEach($compileNodes, function (node) {
                 var directives = collectDirectives(node);
-                applyDirectivesToNode(directives, node);
-                if (node.childNodes && node.childNodes.length) {
+                var terminal = applyDirectivesToNode(directives, node);
+                if (!terminal && node.childNodes && node.childNodes.length) {
                     compileNodes(node.childNodes);
                 }
             });
         }
 
-        function applyDirectivesToNode(directives, compileNode) {
+        function applyDirectivesToNode(directives, compileNode, attrs) {
             var $compileNode = $(compileNode);
+            var terminalPriority = -Number.MAX_VALUE;
+            var terminal = false;
             _.forEach(directives, function (directive) {
+                if (directive.priority < terminalPriority) {
+                    return false;
+                }
+
                 if (directive.compile) {
-                    directive.compile($compileNode);
+                    directive.compile($compileNode, attrs);
+                }
+                if (directive.terminal) {
+                    terminal = true;
+                    terminalPriority = directive.priority;
                 }
             });
+            return terminal;
         }
 
         function collectDirectives(node) {
