b54100d3cb27715f115bf33980a762c4499fff66
蔡浩 <hugohaohao@qq.com>
2019-11-04 19:44:03 +0800

    Added: denomalized name for dom

--- src/compile.js
+++ src/compile.js
@@ -56,14 +56,26 @@ function $CompileProvider($provide) {
 
         function Attributes(element) {
             this.$$element = element;
+            this.$attr = {};
         }
-        Attributes.prototype.$set = function (key, value, writeArr) {
+        Attributes.prototype.$set = function (key, value, writeArr, attrName) {
             this[key] = value;
             if (isBooleanAttribute(this.$$element[0], key)) {
                 this.$$element.prop(key, value);
             }
+            
+            if (!attrName) {
+                if (this.$attr[key]) {
+                    attrName = this.$attr[key];
+                } else {
+                    attrName = this.$attr[key] = _.kebabCase(key, '-');
+                }
+            } else {
+                this.$attr[key] = attrName;
+            }
+
             if (writeArr !== false) {
-                this.$$element.attr(key, value);
+                this.$$element.attr(attrName, value);
             }
         };
 
@@ -146,7 +158,10 @@ function $CompileProvider($provide) {
                             normalizedAttrName[6].toLowerCase() + 
                             normalizedAttrName.substring(7)
                         );
+                        normalizedAttrName = directiveNormalize(name.toLowerCase());
                     }
+                    attrs.$attr[normalizedAttrName] = name;
+
                     var directiveNName = normalizedAttrName.replace(/(Start|End)$/, '');
                     if (directiveIsMultiElement(directiveNName)) {
                         if (/Start$/.test(normalizedAttrName)) {
