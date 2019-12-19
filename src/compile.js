653d4013ae7da92626fcdb6b0a63ac2f3a470627
蔡浩 <hugohaohao@qq.com>
2019-12-19 19:28:10 +0800

    Added: asychronous linking

--- src/compile.js
+++ src/compile.js
@@ -316,14 +316,29 @@ function $CompileProvider($provide) {
                 var templateUrl = _.isFunction(origAsyncDirective.templateUrl) ?
                                     origAsyncDirective.templateUrl($compileNode, attrs) :
                                     origAsyncDirective.templateUrl;
+                var afterTemplateNodeLinkFn, afterTemplateChildLinkFn;
+                var linkQueue = [];
                 $compileNode.empty();
                 $http.get(templateUrl).success(function (template) {
                     directives.unshift(derivedSycDirectives);
                     $compileNode.html(template);
-                    applyDirectivesToNode(
+                    afterTemplateNodeLinkFn = applyDirectivesToNode(
                         directives, $compileNode, attrs, previousCompileContext);
-                    compileNodes($compileNode[0].childNodes);
+                    afterTemplateChildLinkFn = compileNodes($compileNode[0].childNodes);
+                    _.forEach(linkQueue, function (linkCall) {
+                        afterTemplateNodeLinkFn(
+                            afterTemplateChildLinkFn, linkCall.scope, linkCall.linkNode);
+                    });
+                    linkQueue = null;
                 });
+
+                return function delayedNodeLinkFn(_ignoreChildLinkFn, scope, linkNode) {
+                    if (linkQueue) {
+                        linkQueue.push({scope: scope, linkNode: linkNode});
+                    } else {
+                        afterTemplateNodeLinkFn(afterTemplateChildLinkFn, scope, linkNode);
+                    }
+                };
             }
 
             _.forEach(directives, function (directive, i) {
@@ -369,7 +384,7 @@ function $CompileProvider($provide) {
                         throw "Myltiple directives asking for template";
                     }
                     templateDirective = directive;
-                    compileTemplateUrl(
+                    nodeLinkFn = compileTemplateUrl(
                         _.drop(directives, i),
                         $compileNode,
                         attrs,
